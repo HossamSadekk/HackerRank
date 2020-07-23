7c3d059e41784053af0607b22efcaeac48f031b3
HossamSadek <hosssamsadek@gmail.com>
2020-07-23 19:28:38 +0200

    Height of a Binary tree Function

---
+++ Tree Height of a Binary Tree.java
@@ -0,0 +1,15 @@
+public static int height(Node root) {
+      	  int leftHeight = 0;
+    int rightHeight = 0;
+
+    if (root.left != null) {
+        leftHeight = 1 + height(root.left);
+    }
+
+    if (root.right != null) {
+        rightHeight = 1 + height(root.right);
+    }
+
+     return Math.max(leftHeight, rightHeight) ;
+    }
+
