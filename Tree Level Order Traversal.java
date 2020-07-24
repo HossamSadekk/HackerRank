2c787f4aee7f89d3de79a64c30d5c9694ec874c9
HossamSadek <hosssamsadek@gmail.com>
2020-07-24 18:45:25 +0200

    Tree: Level Order Traversal Function

---
+++ Tree Level Order Traversal.java
@@ -0,0 +1,18 @@
+public static void levelOrder(Node root) {
+        
+      Queue<Node> q = new LinkedList<Node>();
+
+      q.add(root);
+
+      while(!q.isEmpty())
+      {   
+          
+          Node current = q.poll();
+
+          System.out.print(current.data+" ");
+
+          if(current.left!=null){q.add(current.left);}
+          if(current.right!=null){q.add(current.right);}
+      }
+      
+    }
