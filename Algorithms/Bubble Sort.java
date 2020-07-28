fdd3050c9eaddbd6bf932d8078f86feba23d765e
HossamSadek <hosssamsadek@gmail.com>
2020-07-28 19:30:53 +0200

    Bubble Sort Algorithm

---
+++ Algorithms/Bubble Sort.java
@@ -0,0 +1,19 @@
+
+static boolean flag = true;
+static void BubbleSort(int [] arr)
+{
+    for(int i=0;i<arr.length-1;i++)
+    {
+        for(int j=0;j<arr.length-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+                flag=false;
+            }
+        }
+        if(flag==true){break;}
+    }
+}
