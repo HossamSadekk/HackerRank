36f4a0a29799a3005522031a4ff71bb548d5a1b0
HossamSadek <hosssamsadek@gmail.com>
2020-08-01 16:51:47 +0200

    the insertionSort1 function [HackerRank]

---
+++ Algorithms/Insertion Sort - Part 1.java
@@ -0,0 +1,24 @@
+static void printarrayay(int array[])
+    {
+    int n = array.length;
+    for (int i=0; i<n; ++i)
+    System.out.print(array[i]+" ");
+    System.out.println();
+    }
+
+    static void insertionSort1(int n, int[] arr) {
+    int key,j;
+    for (int i = 1; i < arr.length; i++) 
+    {
+        key=arr[i];
+        j=i-1;
+        while(j>=0 && arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j-=1;
+            printarrayay(arr); 
+        }
+        arr[j+1]=key;
+    }
+    printarrayay(arr);
+    }
