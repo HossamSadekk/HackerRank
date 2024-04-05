b401895a3a042dc14fc7f26e0065e3720e3071f0
Hossam <66652255+HossamSadekk@users.noreply.github.com>
2024-04-05 20:38:10 +0200

    Create Bill Division.kt

---
+++ Bill Division.kt
@@ -0,0 +1,44 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+
+fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+    val sumOfBarian = bill.sum() - bill[k]
+    val shouldPay = sumOfBarian / 2
+    if(shouldPay == b){
+        print("Bon Appetit")
+    }else{
+        val refund = b - shouldPay
+        print(refund)
+    }
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val k = first_multiple_input[1].toInt()
+
+    val bill = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trim().toInt()
+
+    bonAppetit(bill, k, b)
+}
